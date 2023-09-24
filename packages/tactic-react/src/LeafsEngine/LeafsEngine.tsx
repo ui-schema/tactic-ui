6bb61da0eb2496ee8cde6f4a54c5d06a01ba024f
Michael Becker <michael@bemit.codes>
2023-09-24 22:01:10 +0200

    adj internal React component type; add example custom component;

--- packages/tactic-react/src/LeafsEngine/LeafsEngine.tsx
+++ packages/tactic-react/src/LeafsEngine/LeafsEngine.tsx
@@ -2,7 +2,13 @@ import { ReactDeco } from '@tactic-ui/react/Deco'
 import React from 'react'
 import { LeafsRenderMapping, GenericLeafsDataSpec } from '@tactic-ui/engine/Leaf'
 
-export type ReactLeafDefaultNodeType<P = {}> = (props: P, context?: any) => React.ReactNode
+/**
+ * A wider `React.ComponentType`, as the remapping had a lot of issues when `React.ComponentType` was used internally, somehow not reproducible here or in others with React18.
+ * But in ui-schema with the latest React 18 setup, `React.ComponentType` won't work without the `React.ComponentClass<P>`
+ */
+export type ReactLeafDefaultNodeType<P = {}> =
+    React.ComponentClass<P> |
+    ((props: P, context?: any) => React.ReactNode)
 
 export type ReactLeafsNodeSpec<LDS extends GenericLeafsDataSpec> = {
     [K in keyof LDS]: ReactLeafDefaultNodeType<NonNullable<LDS[K]>>
